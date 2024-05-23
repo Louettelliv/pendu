4a8cab74346196f5b416c04f7caf931716659388
200~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~ <louannevillette2000@gmail.com>
2024-05-23 00:55:28 -0400

    Ajout des commentaires de la fonction pour séloectionner un mot aléatoirement

--- main.py
+++ main.py
@@ -10,10 +10,24 @@ Date: 23/05/2024
 from random import choice
 
 def select_random_word(complete_path_file="mots_pendu.txt"):
+    """
+        Sélectionne un mot aléatoire à partir d'un fichier texte contenant une liste de mots.
+        Paramètres:
+            complete_path_file (str): Chemin complet vers le fichier contenant les mots.
+                                      Par défaut, "mots_pendu.txt".
+        Retours:
+            str: Un mot choisi aléatoirement dans le fichier.
+            list: Une liste vide si le fichier n'est pas trouvé.
+        """
     try:
+        # Tente d'ouvrir le fichier spécifié en mode lecture avec encodage UTF-8
         with open(complete_path_file, 'r', encoding='utf8') as f:
+            # Lit toutes les lignes du fichier et les stocke dans une liste
             words = f.read().splitlines()
+        # Renvoie un mot choisi aléatoirement dans la liste des mots
         return choice(words)
     except FileNotFoundError:
+        # Capture l'exception si le fichier n'est pas trouvé
         print("Fichier non trouvé. Vérifiez le chemin complet du fichier.")
+        # Renvoie une liste vide pour indiquer qu'aucun mot n'a été trouvé
         return []
