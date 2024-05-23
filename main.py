a2cd62df6fd5328f079d693f1aa789a395b031dc
200~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~ <louannevillette2000@gmail.com>
2024-05-23 00:23:38 -0400

    Création du script pour le jeu du pendu

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Script: Jeu du Pendu
+Description: Ce script contient une implémentation du jeu du pendu en Python.
+             Le joueur peut proposer des lettres pour deviner un mot sélectionné au hasard dans un fichier texte
+             jusqu'à ce que le mot soit deviné ou que le nombre maximum de tentatives soit atteint.
+Auteur: Lou-Anne Villette
+Date: 23/05/2024
+"""
